6e686d88db26359197cc8957ed78c32091c07537
Bruno Carvalho <bcs6@cin.ufpe.br>
2025-03-23 11:22:53 -0300

    subindo nova logo

--- frontend/src/app/home/pages/HomePage/index.tsx
+++ frontend/src/app/home/pages/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./index.module.css";
+import logo from '../../../Images/logo.png'; 
 
 const HomePage = () => {
   const navigate = useNavigate(); //Hook para navegação
@@ -12,29 +13,20 @@ const HomePage = () => {
   return (
     <div className={styles.container}>
       {/* Logo do site */}
+      {/* Logo do site - substituindo o SVG por uma imagem */}
       <div className={styles.logo}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
+        <img
+          src={logo} // Usando a imagem importada
+          alt="Logo do Nido"
           width="100"
           height="100"
-          viewBox="0,0,256,256"
-          style={{ mixBlendMode: "normal" }} //Correção do estilo usando objeto
-        >
-          <g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none">
-            <g transform="scale(0.5,0.5)">
-              <path d="M7.9,256c0,-137 111.1,-248.1 248.1,-248.1c137,0 248.1,111.1 248.1,248.1c0,137 -111.1,248.1 -248.1,248.1c-137,0 -248.1,-111.1 -248.1,-248.1z" fill="#eb808c"></path>
-              <path d="M408.1,206.8l-150.1,-74.9c-1.3,-0.6 -2.7,-0.6 -4,0l-150.2,74.9c-1.7,0.8 -2.7,2.4 -2.7,4.3v29.6c0,1.6 0.8,3.1 2.2,4c1.4,0.9 3.1,1 4.5,0.3l148.2,-74l148.1,74c0.6,0.3 1.3,0.5 2,0.5c0.9,0 1.8,-0.2 2.5,-0.7c1.4,-0.9 2.2,-2.4 2.2,-4v-29.8c0,-1.8 -1,-3.5 -2.7,-4.2z" fill="#ffffff"></path>
-              <path d="M380.5,245.8l-124.5,-62l-124.5,62v115h69v-88.2h47.5v88.2h132.5z" fill="#ffffff"></path>
-              <path d="M263.7,272.6h32v32h-32z" fill="#eb808c"></path>
-            </g>
-          </g>
-        </svg>
+          style={{ mixBlendMode: "normal" }}
+        />
       </div>
 
+
       {/* Título e Descrição */}
-      <h1 className={styles.title}>Bem-vindo ao Nido</h1>
+      <h1 className={styles.title}>Bem-vindo ao nido</h1>
       <p className={styles.description}>
         Um lar onde quer que tenhas que estar.
       </p>
--- frontend/src/app/home/pages/SearchResults/index.tsx
+++ frontend/src/app/home/pages/SearchResults/index.tsx
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom";
 import styles from "./index.module.css";
 import ResultCard from "../../../shared/components/ResultCard/ResultCard";
 import { useNavigate } from "react-router-dom";
+import logo from '../../../Images/logo.png'; 
 
 const SearchResults = () => {
     const navigate = useNavigate();
@@ -66,25 +67,14 @@ const query = new URLSearchParams(location.search).toString();
 
   return (
     <div className={styles.container}>
-            <div className={styles.logo}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
+       <div className={styles.logo}>
+        <img
+          src={logo} // Usando a imagem importada
+          alt="Logo do Nido"
           width="100"
           height="100"
-          viewBox="0,0,256,256"
           style={{ mixBlendMode: "normal" }}
-        >
-          <g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none">
-            <g transform="scale(0.5,0.5)">
-              <path d="M7.9,256c0,-137 111.1,-248.1 248.1,-248.1c137,0 248.1,111.1 248.1,248.1c0,137 -111.1,248.1 -248.1,248.1c-137,0 -248.1,-111.1 -248.1,-248.1z" fill="#eb808c"></path>
-              <path d="M408.1,206.8l-150.1,-74.9c-1.3,-0.6 -2.7,-0.6 -4,0l-150.2,74.9c-1.7,0.8 -2.7,2.4 -2.7,4.3v29.6c0,1.6 0.8,3.1 2.2,4c1.4,0.9 3.1,1 4.5,0.3l148.2,-74l148.1,74c0.6,0.3 1.3,0.5 2,0.5c0.9,0 1.8,-0.2 2.5,-0.7c1.4,-0.9 2.2,-2.4 2.2,-4v-29.8c0,-1.8 -1,-3.5 -2.7,-4.2z" fill="#ffffff"></path>
-              <path d="M380.5,245.8l-124.5,-62l-124.5,62v115h69v-88.2h47.5v88.2h132.5z" fill="#ffffff"></path>
-              <path d="M263.7,272.6h32v32h-32z" fill="#eb808c"></path>
-            </g>
-          </g>
-        </svg>
+        />
       </div>
 
       <h1 className={styles.title}>Resultados da Busca</h1>
